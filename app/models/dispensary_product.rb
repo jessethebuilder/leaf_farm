caf898fbf881272c009e3a0e6fd822cd091a762a
Jesse Farmer <jessethebuilder@yahoo.com>
2015-02-11 00:46:09 -0800

    Initial commit

--- app/models/dispensary_product.rb
+++ app/models/dispensary_product.rb
@@ -8,12 +8,11 @@ class DispensaryProduct
     dp.source = 'leafly'
     dp.name = leafly_hash['name']
     dp.description = leafly_hash['description']
-    dp.category = leafly_hash['category']
-    dp.slug = leafly_hash['slug']
-    #[:name, :description, :category, :slug].each{ |attr| dp.send(attr, leafly_hash[:slug]) }
+    dp.category = leafly_hash['type']
     dp.source_image_path = leafly_hash['imagePath']
     dp.pricing_array = leafly_hash['pricing']
     if leafly_hash['strainInfo']
+      dp.slug = leafly_hash['strainInfo']['slug']
       dp.rating = leafly_hash['strainInfo']['rating']
       dp.rating_count = leafly_hash['strainInfo']['ratingCount']
       dp.product_type = leafly_hash['strainInfo']['category']
