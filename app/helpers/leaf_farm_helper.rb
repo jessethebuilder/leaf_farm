89a9747fe20929aa48f05496242fe2c150522b57
Jesse Farmer <jessethebuilder@yahoo.com>
2015-02-13 17:28:40 -0800

    Dispensary Class

--- app/helpers/leaf_farm_helper.rb
+++ app/helpers/leaf_farm_helper.rb
@@ -2,10 +2,16 @@ module LeafFarmHelper
   LEAFLY_ROOT = 'http://www.leafly.com'
 
   def leafly_strain_url(dispensary_product)
-    "#{LEAFLY_ROOT}/#{dispensary_product.categor}/#{dispensary_product.slug}"
+    "#{LEAFLY_ROOT}/#{dispensary_product.category.downcase}/#{dispensary_product.slug}"
   end
 
   def leafly_location_url(slug)
     "http://www.leafly.com/dispensary-info/#{slug}"
+  end
+
+  #-------------Leafly Helpers-----------------------------
+
+  def leafly_logo_link(size = 'large')
+    link_to image_tag(asset_url("leafly/leafly_logo_#{size}.png")), 'http://www.leafly.com', :target => '_blank'
   end
 end
